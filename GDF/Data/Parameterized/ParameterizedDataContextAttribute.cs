d0c46a7a63007e973c7c6ba89f2896ad871c7653
Genery <generydev@gmail.com>
2026-05-03 16:59:17 -0400

    Add Paramaterized Data Context system

---
+++ GDF/Data/Parameterized/ParameterizedDataContextAttribute.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Godot;
+
+namespace GDF.Data.Parameterized;
+
+[AttributeUsage(AttributeTargets.Struct | AttributeTargets.Class, AllowMultiple = true)]
+public class ParameterizedDataContextAttribute : Attribute
+{
+    public string Id;
+
+    public ParameterizedDataContextAttribute(string id)
+    {
+        Id = id;
+    }
+}
+
+public static class ParameterizedDataContexts
+{
+    private static readonly Dictionary<string, Definition> Definitions = new();
+    private static bool _initialized = false;
+    
+    public static string[] GetAllIds()
+    {
+        Initialize();
+        return Definitions.Keys.ToArray();
+    }
+
+    public static bool IsValidId(string id)
+    {
+        return Definitions.ContainsKey(id);
+    }
+
+    public static IDataContext Create(string id)
+    {
+        if (GetDefinitionOrNull(id) is not { } def) return null;
+        
+        return def.Constructor();
+    }
+    public static IDataContext Create(string id, Godot.Collections.Dictionary parameters)
+    {
+        if (GetDefinitionOrNull(id) is not { } def) return null;
+        
+        return parameters == null ? def.Constructor() : def.ParameterizedDictionaryConstructor(parameters);
+    }
+    public static IDataContext Create(string id, Godot.Collections.Array parameters)
+    {
+        if (GetDefinitionOrNull(id) is not { } def) return null;
+        
+        return parameters == null ? def.Constructor() : def.ParameterizedArrayConstructor(parameters);
+    }
+    public static IDataContext CreateV(string id, params Variant[] parameters)
+    {
+        if (GetDefinitionOrNull(id) is not { } def) return null;
+        
+        return parameters == null ? def.Constructor() : def.ParameterizedSystemArrayConstructor(parameters);
+    }
+
+    private static Definition? GetDefinition(string id)
+    {
+        if (GetDefinitionOrNull(id) is { } def) return def;
+        GD.PrintErr($"No such parameterized data context id '{id}'");
+        return null;
+    }
+
+    public static Godot.Collections.Array<Godot.Collections.Dictionary> GetPropertyList(string id)
+    {
+        if (GetDefinitionOrNull(id) is not { } def) return new();
+
+        return def.PropertyList;
+    }
+
+    private static Definition? GetDefinitionOrNull(string id)
+    {
+        Initialize();
+        if (!Definitions.TryGetValue(id, out var def))
+        {
+            return null;
+        }
+
+        return def;
+    }
+
+    public static void Initialize()
+    {
+        if (_initialized) return;
+        _initialized = true;
+        Definitions.Clear();
+        var assembly = Assembly.GetAssembly(typeof(GdfConstants));
+        if (assembly == null) return;
+        foreach (var type in assembly.GetTypes())
+        {
+            foreach (var attr in type.GetCustomAttributes<ParameterizedDataContextAttribute>())
+            {
+                if (!typeof(IDataContext).IsAssignableFrom(type))
+                {
+                    GD.PrintErr($"Invalid ParameterizedDataContext attribute on type {type}: Type must extend {nameof(IDataContext)}");
+                    continue;
+                }
+                string id = attr.Id;
+                if (Definitions.TryGetValue(id, out var existing))
+                {
+                    GD.PrintErr($"Duplicate ParameterizedDataContext ID '{id}': {existing.Type} and {type}");
+                    continue;
+                }
+                var definition = new Definition()
+                {
+                    Type = type,
+                    Attribute = attr
+                };
+                InitializeDefinition(ref definition, id);
+                Definitions[attr.Id] = definition;
+            }
+        }
+    }
+
+    private static void InitializeDefinition(ref Definition definition, string id)
+    {
+        var type = definition.Type;
+        var initializationMethod = typeof(ParameterizedDataContexts)
+            .GetMethod(
+                nameof(InitializeDefinitionGeneric),
+                genericParameterCount: 1,
+                bindingAttr: BindingFlags.Static | BindingFlags.NonPublic,
+                binder: null,
+                types: new Type[] { typeof(Definition).MakeByRefType(), typeof(string) },
+                modifiers: null
+                );
+
+        initializationMethod = initializationMethod.MakeGenericMethod(type);
+
+        var methodParams = new object[] { definition, id };
+        initializationMethod.Invoke(null, methodParams);
+
+        definition = (Definition)methodParams[0];
+    }
+
+    private static void InitializeDefinitionGeneric<T>(ref Definition definition, string id) where T : IDataContext, new()
+    {
+        definition.Constructor = () => new T();
+        
+        var fieldInitializerMethod = typeof(ParameterizedDataContexts)
+            .GetMethod(
+                nameof(InitializeFieldDefinitionGeneric),
+                genericParameterCount: 2,
+                bindingAttr: BindingFlags.Static | BindingFlags.NonPublic,
+                binder: null,
+                types: new Type[] { typeof(FieldInfo) },
+                modifiers: null
+            );
+
+        var propertyNames = new List<string>();
+        var propertySetters = new List<Action<BoxedContext<T>, Variant>>();
+
+        definition.PropertyList = new();
+
+        foreach (var fieldInfo in typeof(T).GetFields())
+        {
+            string name = fieldInfo.Name.ToSnakeCase();
+            propertyNames.Add(name);
+
+            var fieldDef = (Definition.Field<T>)fieldInitializerMethod!
+                .MakeGenericMethod(typeof(T), fieldInfo.FieldType).Invoke(null, new object[] { fieldInfo })!;
+            propertySetters.Add(fieldDef.Setter);
+
+            definition.PropertyList.Add(new Godot.Collections.Dictionary()
+            {
+                {"name", name},
+                {"type", Variant.From(fieldDef.VariantType)}
+            });
+        }
+
+        definition.ParameterizedDictionaryConstructor = (dict) =>
+        {
+            var boxed = new BoxedContext<T>()
+            {
+                Value = new T()
+            };
+
+            if (dict != null)
+            {
+                for (int i = 0; i < propertyNames.Count; i++)
+                {
+                    string name = propertyNames[i];
+                    var setter = propertySetters[i];
+
+                    if (!dict.TryGetValue(name, out var rawValue)) continue;
+                    setter(boxed, rawValue);
+                }
+            }
+            
+            return boxed.Value;
+        };
+
+        definition.ParameterizedArrayConstructor = (arr) =>
+        {
+            var boxed = new BoxedContext<T>()
+            {
+                Value = new T()
+            };
+
+            if (arr != null)
+            {
+                int arrCount = arr.Count;
+                for (var i = 0; i < propertySetters.Count && i < arrCount; i++)
+                {
+                    var setter = propertySetters[i];
+
+                    var rawValue = arr[i];
+                    setter(boxed, rawValue);
+                }
+            }
+            
+            return boxed.Value;
+        };
+
+        definition.ParameterizedSystemArrayConstructor = (arr) =>
+        {
+            var boxed = new BoxedContext<T>()
+            {
+                Value = new T()
+            };
+
+            if (arr != null)
+            {
+                int arrCount = arr.Length;
+                for (var i = 0; i < propertySetters.Count && i < arrCount; i++)
+                {
+                    var setter = propertySetters[i];
+
+                    var rawValue = arr[i];
+                    setter(boxed, rawValue);
+                }
+            }
+            
+            return boxed.Value;
+        };
+    }
+
+    private static Definition.Field<TType> InitializeFieldDefinitionGeneric<TType, [MustBeVariant]TField>(FieldInfo field) where TType : IDataContext, new()
+    {
+        return new Definition.Field<TType>()
+        {
+            Setter = (boxed, value) =>
+            {
+                field.SetValueDirect(__makeref(boxed.Value), value.As<TField>());
+            },
+            VariantType = Variant.From((TField)default).VariantType
+        };
+    }
+
+    private struct Definition
+    {
+        public Type Type;
+        public ParameterizedDataContextAttribute Attribute;
+        
+        public Godot.Collections.Array<Godot.Collections.Dictionary> PropertyList;
+
+        public Func<IDataContext> Constructor;
+        public Func<Godot.Collections.Dictionary, IDataContext> ParameterizedDictionaryConstructor;
+        public Func<Godot.Collections.Array, IDataContext> ParameterizedArrayConstructor;
+        public Func<Variant[], IDataContext> ParameterizedSystemArrayConstructor;
+
+        public struct Field<TType> where TType : IDataContext, new()
+        {
+            public Action<BoxedContext<TType>, Variant> Setter;
+            public Variant.Type VariantType;
+        }
+    }
+
+    private class BoxedContext<T> where T : IDataContext, new()
+    {
+        public T Value;
+    }
+}
---
+++ GDF/Data/Parameterized/ParameterizedDataContextNode.cs
@@ -0,0 +1,236 @@
+﻿using System.Linq;
+using GDF.Editor;
+using Godot;
+using Godot.Collections;
+using Projects.Sample;
+
+namespace GDF.Data.Parameterized;
+
+[Tool]
+[GlobalClass]
+[Icon($"{GdfConstants.IconRoot}/data_context.png")]
+public partial class ParameterizedDataContextNode : Node, IDataContext
+{
+    [Signal]
+    public delegate void UpdatedEventHandler();
+    
+    [Export(PropertyHint.Enum)]
+    public string TypeId
+    {
+        get => _typeId;
+        set
+        {
+            if (_typeId == value) return;
+            _typeId = value;
+#if TOOLS
+            if (!string.IsNullOrEmpty(_typeId) && EditorUtils.IsSettingPropertyThroughInspector(this))
+            {
+                this.Name = _typeId.Capitalize();
+                Params.Clear();
+            }
+#endif
+            DiscardCache();
+            NotifyPropertyListChanged();
+            OnParametersUpdated();
+        }
+    }
+
+    [Export]
+    public Dictionary Params
+    {
+        get => _params;
+        set
+        {
+            if (_params == value) return;
+            _params = value;
+            EnsureParametersUnique();
+            OnParametersUpdated();
+        }
+    }
+
+    private string _typeId = "";
+    
+    private Array<Dictionary> _paramPropertyList = null;
+    private Dictionary _params = new();
+    private IDataContext _context;
+
+    private bool _paramsUnique = false;
+
+    private void DiscardCache()
+    {
+        _paramPropertyList = null;
+    }
+
+    private void EnsureParametersReady()
+    {
+        EnsurePropertyListReady();
+
+        foreach (string key in Params.Keys.ToArray())
+        {
+            var keyValid = false;
+            foreach(var rawParam in _paramPropertyList)
+            {
+                if (rawParam["original_name"].AsString() == key)
+                {
+                    keyValid = true;
+                    break;
+                }
+            }
+
+            if (!keyValid)
+            {
+                Params.Remove(key);
+            }
+        }
+    }
+    
+    private void EnsurePropertyListReady()
+    {
+        if (_paramPropertyList != null) return;
+        
+        UpdatePropertyList();
+    }
+
+    public override bool _Set(StringName property, Variant value)
+    {
+        EnsureParametersReady();
+        foreach (var propertyInfo in _paramPropertyList)
+        {
+            if (propertyInfo["name"].AsStringName() == property)
+            {
+                EnsureParametersUnique();
+                if (value.VariantType != Variant.Type.Nil)
+                    Params[propertyInfo["original_name"].AsString()] = value;
+                else
+                    Params.Remove(propertyInfo["original_name"].AsString());
+                OnParametersUpdated();
+            }
+        }
+        return base._Set(property, value);
+    }
+
+    private void EnsureParametersUnique()
+    {
+        if (_paramsUnique) return;
+        _params = _params?.Duplicate();
+        _paramsUnique = true;
+    }
+
+    public override Variant _Get(StringName property)
+    {
+        EnsureParametersReady();
+        foreach (var propertyInfo in _paramPropertyList)
+        {
+            if (propertyInfo["name"].AsStringName() == property)
+            {
+                return System.Collections.Generic.CollectionExtensions.GetValueOrDefault(Params, propertyInfo["original_name"].AsString());
+            }
+        }
+        return base._Get(property);
+    }
+
+    public override bool _PropertyCanRevert(StringName property)
+    {
+        EnsurePropertyListReady();
+        foreach (var propertyInfo in _paramPropertyList)
+        {
+            if (propertyInfo["name"].AsStringName() == property)
+            {
+                return true;
+            }
+        }
+        return base._PropertyCanRevert(property);
+    }
+
+    public override Variant _PropertyGetRevert(StringName property)
+    {
+        EnsurePropertyListReady();
+        foreach (var propertyInfo in _paramPropertyList)
+        {
+            if (propertyInfo["name"].AsStringName() == property)
+            {
+                return propertyInfo["default"];
+            }
+        }
+        return base._PropertyGetRevert(property);
+    }
+
+    public override Array<Dictionary> _GetPropertyList()
+    {
+        UpdatePropertyList();
+        var arr = new Array<Dictionary>();
+        arr.AddRange(_paramPropertyList);
+        return arr;
+    }
+
+    private void UpdatePropertyList()
+    {
+        _paramPropertyList ??= new();
+        _paramPropertyList.Clear();
+        if (!string.IsNullOrEmpty(_typeId) && ParameterizedDataContexts.IsValidId(_typeId))
+        {
+            foreach (var rawProperty in ParameterizedDataContexts.GetPropertyList(_typeId))
+            {
+                string originalName = rawProperty["name"].AsString();
+                var newName = $"params/{originalName}";
+                var newProp = new Godot.Collections.Dictionary()
+                {
+                    {"name", newName},
+                    {"type", rawProperty["type"]},
+                    {"usage", Variant.From(PropertyUsageFlags.Editor)},
+                    {"hint", Variant.From(PropertyHint.None)},
+                    {"hint_string", ""},
+                    {"original_name", originalName},
+                    {"default", (Variant)default}
+                };
+                _paramPropertyList.Add(newProp);
+            }
+        }
+    }
+
+    public override void _ValidateProperty(Dictionary property)
+    {
+        var propertyName = property["name"].AsStringName();
+        var usage = property["usage"].As<PropertyUsageFlags>();
+        if (propertyName == PropertyName.TypeId)
+        {
+#if TOOLS
+            property["hint_string"] = ParameterizedDataContexts.GetAllIds()
+                .OrderBy((id) => ParameterizedDataContexts.GetPropertyList(id).Count).ToArray().Join(",");
+#endif
+        }
+        if (propertyName == PropertyName.Params)
+        {
+            usage &= ~(PropertyUsageFlags.Editor);
+        }
+
+        property["usage"] = Variant.From(usage);
+        base._ValidateProperty(property);
+    }
+
+    private void OnParametersUpdated()
+    {
+        _context?.DisconnectUpdateSignal(new Callable(this, MethodName.OnContextUpdated));
+        _context = ParameterizedDataContexts.Create(TypeId, Params);
+        _context?.ConnectUpdateSignal(new Callable(this, MethodName.OnContextUpdated));
+        EmitSignalUpdated();
+    }
+
+    private void OnContextUpdated()
+    {
+        EmitSignalUpdated();
+    }
+
+    public StringName UpdatedSignalName => SignalName.Updated;
+
+    public IDataContext ParentContext => _context;
+
+    public override void _Notification(int what)
+    {
+        base._Notification(what);
+        if (what == NotificationPredelete)
+        {
+            _context?.DisconnectUpdateSignal(new Callable(this, MethodName.OnContextUpdated));
+        }
+    }
+}
