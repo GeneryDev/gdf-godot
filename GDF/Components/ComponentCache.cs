acc15bf809ba450267748074e79dbcd577a1fcf8
Genery <generydev@gmail.com>
2026-05-10 20:24:43 -0400

    Optimize component cache lookups when component is missing

--- GDF/Components/ComponentCache.cs
+++ GDF/Components/ComponentCache.cs
@@ -7,6 +7,7 @@ public struct ComponentCache<T> where T : class
 {
     private Node _source;
     private T _cache;
+    private bool _everRequestedInTree;
 
     public ComponentCache()
     {
@@ -16,14 +17,22 @@ public struct ComponentCache<T> where T : class
     {
         _source = source;
     }
+
+    public void Clear()
+    {
+        _source = null;
+        _cache = null;
+        _everRequestedInTree = false;
+    }
     
     public T Get(Node source)
     {
-        if (_source != source) _cache = null;
+        if (_source != source) Clear();
         _source = source;
-        if (_cache != null) return _cache;
+        if (_cache != null || _everRequestedInTree) return _cache;
         if (source == null) return null;
         _cache = _source.GetComponent<T>();
+        _everRequestedInTree |= source.IsInsideTree();
         return _cache;
     }
 }
