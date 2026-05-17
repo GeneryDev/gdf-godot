cf27645039acf1f2a758dadb5046fa091019d48f
Genery <generydev@gmail.com>
2026-05-16 20:51:14 -0400

    Add read-only Component Owner property in ComponentIndex

--- GDF/Components/ComponentIndex.cs
+++ GDF/Components/ComponentIndex.cs
@@ -1,19 +1,29 @@
 ﻿using System.Collections.Generic;
 using GDF.Util;
 using Godot;
+using Godot.Collections;
 
 namespace GDF.Components;
 
+[Tool]
 [GlobalClass]
 [Icon($"{GdfConstants.IconRoot}/component_index.png")]
 public partial class ComponentIndex : Node
 {
     [Export]
-    public Node[] OtherComponentContainers;
+    public Node ComponentOwner
+    {
+        get => GetParent();
+        // ReSharper disable once ValueParameterNotUsed
+        set {}
+    }
+
+    [Export]
+    public Node[] OtherComponentContainers = System.Array.Empty<Node>();
     
     public T GetComponent<T>() where T : class
     {
-        if (GetParent()?.GetChildOfType<T>() is { } siblingComp) return siblingComp;
+        if (ComponentOwner?.GetChildOfType<T>() is { } siblingComp) return siblingComp;
         if (OtherComponentContainers != null)
         {
             foreach (var container in OtherComponentContainers)
@@ -25,7 +35,7 @@ public partial class ComponentIndex : Node
     }
     public List<T> GetComponents<T>(List<T> output) where T : class
     {
-        if (GetParent() is { } parent)
+        if (ComponentOwner is { } parent)
         {
             foreach (var sibling in parent.IterateChildrenOfType<T>())
             {
@@ -45,4 +55,20 @@ public partial class ComponentIndex : Node
         }
         return output;
     }
+
+
+#if TOOLS
+    public override void _ValidateProperty(Dictionary property)
+    {
+        var propName = property["name"].AsStringName();
+        var usage = property["usage"].As<PropertyUsageFlags>();
+
+        if (propName == PropertyName.ComponentOwner)
+        {
+            usage |= PropertyUsageFlags.ReadOnly;
+        }
+
+        property["usage"] = Variant.From(usage);
+    }
+#endif
 }
