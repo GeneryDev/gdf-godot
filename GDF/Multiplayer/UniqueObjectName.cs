6c6ee82afca9b0fbda100e43d83758988958aa0b
Genery <generydev@gmail.com>
2026-05-24 16:33:18 -0400

    Create UniqueObjectName.cs

---
+++ GDF/Multiplayer/UniqueObjectName.cs
@@ -0,0 +1,15 @@
+﻿using GDF.Logical.Values;
+using Godot;
+
+namespace GDF.Multiplayer;
+
+[GlobalClass]
+public partial class UniqueObjectName : ValueSource
+{
+    [Export] public string Label = "Object";
+    
+    public override Variant GetValue(Node source)
+    {
+        return Room.Instance.GenerateUniqueObjectName(Label);
+    }
+}
