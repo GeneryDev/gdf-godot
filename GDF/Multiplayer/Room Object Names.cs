048d79cbb6fba84f8bd56a442e9bc6fb2e182a18
Genery <generydev@gmail.com>
2026-05-24 13:56:49 -0400

    Create Room Object Names.cs

---
+++ GDF/Multiplayer/Room Object Names.cs
@@ -0,0 +1,11 @@
+﻿namespace GDF.Multiplayer;
+
+public partial class Room
+{
+    private int _nextObjectId = 0;
+
+    public string ClaimNewObjectName(string type = null)
+    {
+        return $"{type ?? "Object"} [{PeerId:X}_{_nextObjectId++}]";
+    }
+}
