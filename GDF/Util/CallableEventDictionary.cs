c566fa5aa2b0522efee71d1c8f2bfc82f422d409
GeneryDev <generydev@gmail.com>
2026-07-13 22:40:51 -0400

    Add CallableEventDictionary

---
+++ GDF/Util/CallableEventDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Godot;
+
+namespace GDF.Util;
+
+public struct CallableEventDictionary<TKey>
+{
+    private Dictionary<TKey, CallableEvent> _events;
+
+    public void Connect(TKey key, Callable callable)
+    {
+        _events ??= new();
+        if (!_events.TryGetValue(key, out var events))
+        {
+            events = new CallableEvent();
+        }
+        events.Connect(callable);
+        _events[key] = events;
+    }
+    public void Disconnect(TKey key, Callable callable)
+    {
+        if (_events == null) return;
+        if (!_events.TryGetValue(key, out var events))
+        {
+            events = new CallableEvent();
+        }
+        events.Disconnect(callable);
+        _events[key] = events;
+    }
+
+    public void Invoke(TKey key, params Variant[] args)
+    {
+        if (_events == null) return;
+        if (_events.TryGetValue(key, out var events))
+        {
+            events.Invoke(args);
+        }
+    }
+}
